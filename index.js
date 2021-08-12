d3cf3fcfb2c3a4d1d162bdc5aacc7ad5ad5766fa
Wilfred <wilfredbayudan@gmail.com>
2021-08-11 21:32:31 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(array) {
+  function findWin(element) {
+    return (element.result === "W");
+  }
+  const found = array.find(findWin);
+  return found ? found.year : undefined;
+}
